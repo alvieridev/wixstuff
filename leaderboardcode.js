caff60b447ae0aef2a935398c0d4b626ff7d4cfc
ALVIERI <119531683+alvieridev@users.noreply.github.com>
2024-08-03 15:10:44 +0100

    Create leaderboardcode.js

---
+++ leaderboardcode.js
@@ -0,0 +1,88 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import {formatLeaderBoardRowsAndColumns} from "backend/googlesheets"
+import {getRacePointsRowsAndColumns} from "backend/googlesheets"
+import {getLeaderBoardValuesFromSheet} from "backend/googlesheets"
+
+const populateLeaderBoardTable = async (gender) =>{
+
+	const leaderBoardData = await getLeaderBoardValuesFromSheet();
+
+	const rows = leaderBoardData.data.values
+
+	const headers = rows[0];
+	let formattedColumns = []
+
+	headers.forEach((header) => {
+		const column = {
+			id: header,
+			dataPath: header,
+			label: header,
+			width: 100,
+			visible: true,
+			type: "string",
+			linkPath: "link-field-or-property",
+		}
+		formattedColumns.push(column)
+	})
+
+	let formattedRows = []
+	for( let i = 1; i < rows.length; i++){
+		const row = {
+
+		}
+		for(let j = 0; j < headers.length; j++){
+			let rowValue =rows[i][j]
+			
+			if(!(rows[i][j])){
+				rowValue = "not specified"
+			}
+			row[headers[j]] = rowValue;
+		}
+		// console.log("Row array: ", row.Gender)
+
+		if(gender !== 'all' && row.Gender !== `${gender}`){
+			continue;
+		}
+		formattedRows.push(row)
+	}
+		
+
+	// return {
+	//     formattedRows, 
+	//     formattedColumns
+	// }
+
+
+
+	// const leaderboardata = await formatLeaderBoardRowsAndColumns()
+	$w('#leaderboardTable').columns = formattedColumns
+	$w('#leaderboardTable').rows = formattedRows
+	// $w('#leaderboardTable').columns = leaderboardata.formattedColumns
+	// $w('#leaderboardTable').rows = leaderboardata.formattedRows
+
+
+	// const racepointsdata = await getRacePointsRowsAndColumns()
+	// $w('#racepointstable').columns = racepointsdata.formattedColumns
+	// $w('#racepointstable').rows = racepointsdata.formattedRows
+
+
+
+
+}
+
+
+$w.onReady(function () {
+
+populateLeaderBoardTable("all")
+
+});
+
+
+export function genderDropdown_change(event) {
+	let selectedIndex = $w('#genderDropdown').selectedIndex
+	let selectedValue = $w('#genderDropdown').options[selectedIndex].value
+
+	console.log("Selected value: ",selectedValue)
+	
+	populateLeaderBoardTable(selectedValue)
+}
